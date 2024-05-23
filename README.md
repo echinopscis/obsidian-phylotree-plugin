082b5def892267f58f040fb4a6ea5d26b2ee8904
Nicky Nicolson <nicky.nicolson@gmail.com>
2024-05-23 22:52:29 +0100

    initial revision

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-# Obsidian Wikidata Sidebar Plugin
+# Obsidian Phylotree Plugin
 
-Load data from wikidata to a sidebar, using a direct text search or via mapped frontmatter properties.
+Display phylogenetic trees in Obsidian. Tree data is specified in a fenced code block in newick or nexus format, and the tree is visualised using the phylotree library.
 
 <br>
 
 ## Changelog
 
-### 0.2.0
-- Use selected text as search input, if available ([issue #3](https://github.com/echinopscis/obsidian-wikidata-plugin/issues/3)).
-- Add bracketed description text in entity selection list, if available ([issue #4](https://github.com/echinopscis/obsidian-wikidata-plugin/issues/4))
-- Correct display in select modal when called from frontmatter (shows full URL for entity id) ([issue #5](https://github.com/echinopscis/obsidian-wikidata-plugin/issues/5))
-- Bypass select modal if only one item found ([issue #6](https://github.com/echinopscis/obsidian-wikidata-plugin/issues/6))
- 
 ### 0.1.0 Initial revision
 
 ### Features
 
-- Specify frontmatter mappings (eg DOI - [P356](https://www.wikidata.org/wiki/Property:P356))
-- Specify frontmatter property that holds a wikidata entity ID (Q-number)
-- Simple text search
-- Navigation through linked wikidata entities in the sidebar
-- Links to external conent (to wikidata and using identifiers attached to an entity
+- Adds newick and nexus fenced code blocks to Obsidian to specify phylogenetic trees for visualisation
+- Node click constructs a search for the node labels included in the clade
 
 ## Demo
 
@@ -32,41 +23,16 @@ TBC
 
 ## How to install
 
-Download the zip file named "wikidata-sidebar-plugin.zip" attached to the [latest release](https://github.com/echinopscis/obsidian-wikidata-plugin/releases/latest) and unzip in under the `.obsidian/plugins` directory under your Obsidian vault.
+Download the zip file named "phylotree-plugin.zip" attached to the [latest release](https://github.com/echinopscis/obsidian-wikidata-plugin/releases/latest) and unzip in under the `.obsidian/plugins` directory under your Obsidian vault.
 
 ## How to use
 
-### 1. To use frontmatter properties to retrieve associated wikidata entities
-Click the ribbon icon 🌍, or excute the command "Wikidata sidebar: Load wikidata from frontmatter".
+Create a fenced code block containing phylogenetic tree data, labelled either "newick" or "nexus" depending on the tree format used. Switch to view mode and you should see a visual representation of the tree. The visualisation is interactive: clicking on a node will execute a search across the Obsidian vault for all included node labels.
 
-### 2. To search wikidata
-Use the command "Wikidata sidebar: Search wikidata for sidebar".
-
-### 3. Select the wikidata entity from the search results.
-
-### 4. The entity will be loaded in a sidebar.
-
-## How to use settings
-
-### Frontmatter property mapping
-
-By default this is:
-```
-{
-  orcid: 'P496',
-  doi: 'P356',
-  ipni_name_id: 'P961',
-  ihcode: 'P5858',
-}
-```
-
-### Frontmatter property for the wikidata entity ID
-
-This defaults to `wikidata_entity_id`
 
 ## License
 
-[Wikidata sidebar plugin](https://github.com/echinopscis/obsidian-wikidata-plugin) is licensed under the MIT license. Refer to [LICENSE](/LICENSE.TXT) for more information.
+[Phylotree plugin](https://github.com/echinopscis/obsidian-phylotree-plugin) is licensed under the MIT license. Refer to [LICENSE](/LICENSE.TXT) for more information.
 
 <br>
 
@@ -74,10 +40,10 @@ This defaults to `wikidata_entity_id`
 
 Feel free to contribute.
 
-You can create an [issue](https://github.com/echinopscis/obsidian-wikidata-plugin/issues) to report a bug, suggest an improvement for this plugin, ask a question, etc.
+You can create an [issue](https://github.com/echinopscis/obsidian-phylotree-plugin/issues) to report a bug, suggest an improvement for this plugin, ask a question, etc.
 
-You can make a [pull request](https://github.com/echinopscis/obsidian-wikidata-plugin/pulls) to contribute to this plugin development.
+You can make a [pull request](https://github.com/echinopscis/obsidian-phylotree-plugin/pulls) to contribute to this plugin development.
 
 ## Credits 
 
-Sincere thanks to Toby Hudson (AKA [99of9](https://github.com/99of9)) for his work on the browser plugin [Entity Explosion](https://github.com/99of9/Entity-Explosion) which inspired this plugin, and to [anpigon](https://github.com/anpigon) who developed the [obsidian book search plugin](https://github.com/anpigon/obsidian-book-search-plugin) which was used as the basis for this development.
+Thanks to the developers of phylotree, on which this plugin relies.
--- src/main.ts
+++ src/main.ts
@@ -1,177 +1,54 @@
-import { Notice, Plugin, WorkspaceLeaf } from 'obsidian';
-import { WikidataListView, VIEW_TYPE_EXAMPLE } from '@views/wikidata_sidebar';
-import { WikidataSearchModal } from '@views/wikidata_search_modal';
-import { WikidataSuggestModal } from '@views/wikidata_suggest_modal';
-import { WikidataEntity } from '@models/wikidata.model';
-import {
-  WikidataSidebarPluginSettingTab,
-  WikidataSidebarPluginSettings,
-  DEFAULT_SETTINGS,
-} from '@settings/wikidata_settings';
-import { BaseWikidataApiImpl, factoryServiceProvider } from '@apis/base_api';
+import { App, Plugin, MarkdownPostProcessorContext } from 'obsidian';
+import * as phylotree from 'phylotree';
 
-export default class WikidataSidebarPlugin extends Plugin {
-  settings: WikidataSidebarPluginSettings;
-  serviceProvider: BaseWikidataApiImpl;
-
+export default class PhyloPlugin extends Plugin {
   async onload() {
-    await this.loadSettings();
-
-    this.serviceProvider = factoryServiceProvider();
-
-    console.log('registering view');
-    this.registerView(VIEW_TYPE_EXAMPLE, leaf => new WikidataListView(leaf, this));
-
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon('globe', 'Consult wikidata', () => this.useFrontmatter());
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass('wikidata-sidebar-plugin-ribbon-class');
-
-    this.addCommand({
-      id: 'open-wikidata-search-modal',
-      name: 'Search wikidata for sidebar',
-      callback: () => this.searchForWikidataSidebar(),
-    });
-
-    this.addCommand({
-      id: 'use-mapped-frontmatter',
-      name: 'Load wikidata from frontmatter',
-      callback: () => this.useFrontmatter(),
-    });
-
-    // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new WikidataSidebarPluginSettingTab(this.app, this));
-
-    console.log(
-      `Wikidata sidebar: version ${this.manifest.version} (requires obsidian ${this.manifest.minAppVersion})`,
-    );
+    this.registerMarkdownCodeBlockProcessor('newick', this.phyloCodeBlockProcessor(this.app, 'nwk'));
+    this.registerMarkdownCodeBlockProcessor('nexus', this.phyloCodeBlockProcessor(this.app, 'nexus'));
   }
 
-  showNotice(message: unknown) {
-    try {
-      new Notice(message?.toString());
-    } catch {
-      // eslint-disable
-    }
-  }
+  onunload() {}
 
-  async loadSettings() {
-    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-  }
-
-  async saveSettings() {
-    await this.saveData(this.settings);
-  }
-
-  async searchForWikidataSidebar(): Promise<void> {
-    let item: WikidataEntity;
-    const query = window.getSelection().toString();
-    console.log(query);
-    if (typeof query == 'string') {
-      item = await this.searchWikidataMetadata(query);
-    } else {
-      item = await this.searchWikidataMetadata(query);
-    }
-    this.loadWikidataSidebar(item.entityId);
-  }
-
-  async useFrontmatter(): Promise<void> {
-    const currentPage = this.app.workspace.getActiveFile();
-    const fmc = this.app.metadataCache.getFileCache(currentPage)?.frontmatter;
-    console.log(fmc);
-
-    if (fmc.hasOwnProperty(this.settings.frontmatter_wikidata_entity_property_name)) {
-      const entityId = fmc[this.settings.frontmatter_wikidata_entity_property_name];
-      this.loadWikidataSidebar(entityId);
-    } else {
-      let fmValue;
-      let wdProperty;
-      for (const [fmkey, fmvalue] of Object.entries(fmc)) {
-        if (this.settings.frontmatter2wdpropertymapper.hasOwnProperty(fmkey.toLowerCase())) {
-          fmValue = fmvalue;
-          wdProperty = this.settings.frontmatter2wdpropertymapper[fmkey.toLowerCase()];
-        }
-      }
-      console.log(`Found mapping of ${wdProperty}, looking up value ${fmValue}`);
-      const item = await this.searchWikidataMetadataByProperty(wdProperty, fmValue);
-      const entityId = item.entityId.split('/').pop();
-      console.log(entityId);
-      this.loadWikidataSidebar(entityId);
-    }
-  }
-
-  async loadWikidataSidebar(entityId: string): Promise<void> {
-    try {
-      const myEntityId = entityId;
-      const wikidataItemDesc = await this.serviceProvider.getEntityDescription(myEntityId);
-      console.log(wikidataItemDesc);
-      const wikidataItemDescStatements = await this.serviceProvider.getEntityStatements(myEntityId);
-      console.log(wikidataItemDescStatements);
-      const wikidataItemDescLinks = await this.serviceProvider.getEntityLinks(myEntityId);
-      console.log(wikidataItemDescLinks);
-
-      console.log('Now should load sidebar view');
-      const { workspace } = this.app;
-
-      let leaf: WorkspaceLeaf | null = null;
-      const leaves = workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE);
-
-      if (leaves.length > 0) {
-        console.log('A leaf with our view already exists, use that');
-        leaf = leaves[0];
-      } else {
-        console.log('Our view could not be found in the workspace, create a new leaf in the right sidebar for it');
-        leaf = workspace.getRightLeaf(false);
-        await leaf.setViewState({ type: VIEW_TYPE_EXAMPLE, active: true });
-      }
-      if (leaf.view instanceof WikidataListView) {
-        console.log('Leaf is WikidataListView');
-        leaf.view.setWikidataData(this, wikidataItemDesc, wikidataItemDescStatements, wikidataItemDescLinks);
-        workspace.revealLeaf(leaf);
-      } else {
-        console.log('what is the leaf?');
-      }
-    } catch (err) {
-      console.warn(err);
-      this.showNotice(err);
-    }
-  }
-
-  async searchByEntityId(entityId: string) {
-    console.log(`searching for ${entityId}`);
-    this.loadWikidataSidebar(entityId);
-  }
-
-  async searchWikidataMetadata(query?: string): Promise<WikidataEntity> {
-    const searchResults = await this.openWikidataSearchModal(query);
-    console.log(`Found ${searchResults.length}`);
-    console.log('hello');
-    return await this.openWikidataSuggestModal(searchResults);
-  }
-
-  async openWikidataSearchModal(query = ''): Promise<WikidataEntity[]> {
-    return new Promise((resolve, reject) => {
-      return new WikidataSearchModal(this, query, (error, results) => {
-        return error ? reject(error) : resolve(results);
-      }).open();
-    });
-  }
-
-  async searchWikidataMetadataByProperty(propertyname: string, propertyvalue: string): Promise<WikidataEntity> {
-    const searchResults = await this.serviceProvider.getByProperty(propertyname, propertyvalue);
-    console.log(`Found ${searchResults.length}`);
-    if (searchResults.length == 1) {
-      return searchResults[0];
-    } else {
-      return await this.openWikidataSuggestModal(searchResults);
-    }
-  }
-
-  async openWikidataSuggestModal(items: WikidataEntity[]): Promise<WikidataEntity> {
-    return new Promise((resolve, reject) => {
-      return new WikidataSuggestModal(this.app, items, (error, selectedItem) => {
-        return error ? reject(error) : resolve(selectedItem);
-      }).open();
-    });
+  phyloCodeBlockProcessor(app: App, phyloTreeFormat: string) {
+    return (newick: string, el: HTMLElement, _: MarkdownPostProcessorContext) => {
+      const tree_container = el.createEl('div');
+      tree_container.className = 'tree-container';
+      tree_container.id = 'tree-container';
+      const tree = new phylotree.phylotree(newick, { type: phyloTreeFormat });
+      const renderOptions = {
+        container: tree_container,
+        height: tree_container.clientHeight,
+        width: tree_container.clientWidth,
+        'left-right-spacing': 'fit-to-size',
+        'top-bottom-spacing': 'fit-to-size',
+        selectable: true,
+        'restricted-selectable': false,
+        'binary-selectable': false,
+        collapsible: false,
+        transitions: false,
+        'show-scale': false,
+        'align-tips': false,
+        zoom: false,
+        reroot: false,
+        hide: false,
+        brush: false,
+        branches: 'step',
+        'label-nodes-with-name': true,
+        'internal-names': true,
+        'node-styler': function (element: HTMLElement, data: phylotree.Node) {
+          element.on('click', function () {
+            if (tree.isLeafNode(data)) {
+              app['internalPlugins'].plugins['global-search'].instance.openGlobalSearch(data.data.name);
+            } else {
+              const sel = tree.selectAllDescendants(data, true, true);
+              const nodeLabels = sel.filter(obj => obj.data.name.length > 0).map(obj => obj.data.name);
+              app['internalPlugins'].plugins['global-search'].instance.openGlobalSearch(nodeLabels.join(' OR '));
+            }
+          });
+        },
+      };
+      const rendered_tree = tree.render(renderOptions);
+      tree_container.append(rendered_tree.show());
+    };
   }
 }
